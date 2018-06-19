42258f0abef7fc10102c8dd11c1bf1a49add032c
mahi-a <anethalamahesh@gmail.com>
2018-06-19 16:08:47 +0530

    integrating devise token auth and rubocop

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :confirmable
+  include DeviseTokenAuth::Concerns::User
+
+  has_many :expenditures
+end
