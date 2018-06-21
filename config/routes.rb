c78ca00d9a67154d84017d3e4429be39d9b64e5d
mahi-a <anethalamahesh@gmail.com>
2018-06-21 18:49:08 +0530

    Fixing rubocop issues

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ PropertyAssistant::Application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :expenditures, only: [:create, :update] do
+      resources :expenditures, only: %i[create update] do
       end
 
       get :sub_expenditure_dates, to: 'sub_expenditures#sub_expenditure_dates'
